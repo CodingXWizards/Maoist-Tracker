2e9a33a27a1d80e28e5ed5e7c05544222fe992aa
Raja Singh <rajasgh18@gmail.com>
2024-06-24 01:17:09 +0530

    reactjs files

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BottomBar } from "@/components/bottom-bar";
+import { Sidebar } from "@/components/sidebar";
+import { TableArea } from "@/components/table-area";
+
+const App = () => {
+  return (
+    <main className="flex h-screen w-full">
+      <Sidebar />
+      <section className="flex flex-col w-[calc(100vw-240px)] h-full">
+        <TableArea />
+        {/* <BottomBar /> */}
+      </section>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+
+import App from '@/App.tsx'
+import store from '@/redux/store.ts'
+import '@/index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+)
+
+// Use contextBridge
+window.ipcRenderer.on('main-process-message', (_event, message) => {
+  console.log(message)
+})
