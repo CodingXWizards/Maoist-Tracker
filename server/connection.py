9fd9c55b0812be4598212e40745dcf96e8c1b96a
Raja Singh <rajasgh18@gmail.com>
2024-06-24 01:14:27 +0530

    Database Connection

---
+++ server/connection.py
@@ -0,0 +1,16 @@
+# app/database.py
+
+import pyodbc
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def get_db_instance():
+    server = os.getenv("DATABASE_SERVER")
+    database = os.getenv("DATABASE_NAME")
+    username = os.getenv("DATABASE_USER")
+    password = os.getenv("DATABASE_PASSWORD")
+
+    conn = pyodbc.connect(f"DRIVER=ODBC Driver 17 for SQL Server;SERVER={server};DATABASE={database};UID={username};PWD={password}")
+    return conn
