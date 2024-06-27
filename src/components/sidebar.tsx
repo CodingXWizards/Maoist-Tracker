0e2797d1f5571a1c4e8a56d57020e96994c4c327
Raja Singh <rajasgh18@gmail.com>
2024-06-27 19:59:37 +0530

    updated sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,23 +1,19 @@
-import { useEffect, useState } from "react";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger
-} from "@/components/ui/select"
+import { FormEvent, useEffect, useState } from "react";
 
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { fetchDatabases } from "@/redux/reducers/databaseSlice";
 import { setDatabase, setTable } from "@/redux/reducers/infoSlice";
-import { SelectGroup } from "@radix-ui/react-select";
 import { fetchTableNames } from "@/redux/reducers/tableNameSlice";
 import { Button } from "./ui/button";
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "./ui/dropdown-menu";
+import { generateSummaryCDR, setTableData } from "@/redux/reducers/tableSlice";
+import { cn } from "@/lib/utils";
 
 export const Sidebar = () => {
 
   const { loading: loadingDatabase, databases } = useAppSelector(state => state.databases);
   const { loading: loadingTables, tableNames } = useAppSelector(state => state.tableName);
+  const { loading: loadingData } = useAppSelector(state => state.table);
 
   const { currDatabase, selectedTables } = useAppSelector(state => state.info);
   const [type, setType] = useState<string>('tower');
@@ -44,8 +40,12 @@ export const Sidebar = () => {
     dispatch(setTable(selectedTabl)); // Update this if you need to handle multiple selections differently
   };
 
-  const handleSubmit = () => {
-    
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (currDatabase && selectedTables) {
+      dispatch(generateSummaryCDR({ databaseName: currDatabase, tableNames: selectedTables }));
+    }
   }
 
   return (
@@ -63,34 +63,40 @@ export const Sidebar = () => {
       <DropdownMenu>
         <DropdownMenuTrigger className="w-full bg-primary text-primary-foreground p-2 px-3 rounded-md">{loadingDatabase === 'Pending' ? "loading..." : currDatabase?.split("_").splice(-1) || "Select Database"}</DropdownMenuTrigger>
         <DropdownMenuContent className="max-h-60 overflow-auto">
+          {loadingDatabase === 'Pending' && (<DropdownMenuItem>Loading...</DropdownMenuItem>)}
+          {loadingDatabase === 'Rejected' && (<DropdownMenuItem>Error Loading...</DropdownMenuItem>)}
           {loadingDatabase === 'Fullfilled' && (
             databases.length === 0
               ? <DropdownMenuItem>No Databases</DropdownMenuItem>
               : databases.map((database: string, index: number) => (
-                database.split("_").includes(type) && <DropdownMenuItem key={index} onClick={() => dispatch(setDatabase(database))}>{database.split("_").splice(-1)}</DropdownMenuItem>
+                database.split("_").includes(type) && <DropdownMenuItem key={index} onClick={() => { dispatch(setDatabase(database)); dispatch(setTable(null)); dispatch(setTableData([])) }}>{database.split("_").splice(-1)}</DropdownMenuItem>
               ))
           )}
         </DropdownMenuContent>
       </DropdownMenu>
-      <Select disabled={loadingTables === 'Pending'} onValueChange={handleValueChange}>
-        <SelectTrigger className="bg-primary text-primary-foreground p-2 px-3 rounded-md">
+      <DropdownMenu>
+        <DropdownMenuTrigger className="w-full bg-primary text-primary-foreground p-2 px-3 rounded-md">
           {loadingTables === 'Pending' ? "loading..." : 'Select Tables'}
-        </SelectTrigger>
-        <SelectContent className="max-h-60 overflow-auto">
-          <SelectGroup>
-            {loadingTables === 'Fullfilled' && (
-              tableNames.length === 0
-                ? <SelectItem value="">No Table</SelectItem>
-                : tableNames.map((tableName: string, index: number) => (
-                  <SelectItem value={tableName} key={index}>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="max-h-60 overflow-auto space-y-1">
+          {loadingTables === 'Idle' && <DropdownMenuItem>No Tables</DropdownMenuItem>}
+          {loadingTables === 'Pending' && (<DropdownMenuItem>Loading...</DropdownMenuItem>)}
+          {loadingTables === 'Rejected' && (<DropdownMenuItem>Error Loading...</DropdownMenuItem>)}
+          {loadingTables === 'Fullfilled' && (
+            tableNames.length !== 0
+              ? <>
+                <DropdownMenuItem onClick={() => dispatch(setTable(tableNames))}>All Tables</DropdownMenuItem>
+                {tableNames.map((tableName: string, index: number) => (
+                  <DropdownMenuItem key={index} onClick={() => handleValueChange(tableName)} className={cn(selectedTables?.includes(tableName) && "bg-slate-800 text-white focus:bg-slate-800 focus:bg-opacity-95 focus:text-white")}>
                     {tableName.split("_").splice(-1)}
-                  </SelectItem>
-                ))
-            )}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-      <Button type="submit" className="w-full">Generate Summary</Button>
+                  </DropdownMenuItem>
+                ))}
+              </>
+              : <DropdownMenuItem>No Tables</DropdownMenuItem>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <Button type="submit" className="w-full text-base">{loadingData === 'Pending' ? "Loading..." : "Generate Summary"}</Button>
     </form>
   );
 };
